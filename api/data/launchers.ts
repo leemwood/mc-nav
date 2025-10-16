3b69099442046e0ace3d1f35a6a489d17cd0e908
leemwood <3436464181qq.com>
2025-10-17 00:06:44 +0800

    删除无用文件

--- api/data/launchers.ts
+++
@@ -1,9 +0,0 @@
-import launchersData from './launchers.json'
-
-export async function GET() {
-  return new Response(JSON.stringify(launchersData), {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-}
